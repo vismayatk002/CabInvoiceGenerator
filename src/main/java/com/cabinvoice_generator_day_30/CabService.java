a6463fcd4cab02f99b5785e8b158d98e12c5b8e5
vismayatk002 <vismayatk002@gmail.com>
2021-08-29 16:32:24 +0530

    Updated with final variable

--- src/main/java/com/cabinvoice_generator_day_30/CabService.java
+++ src/main/java/com/cabinvoice_generator_day_30/CabService.java
@@ -7,6 +7,8 @@ public class CabService {
 	float distance;
 	float time;
 	float totalFare;
+	int PRICE_PER_KM = 10;
+	int PRICE_PER_MIN = 1;
 	int MIN_FARE = 5;
 	
     public CabService(float distance, float time) {
@@ -21,16 +23,18 @@ public class CabService {
     	}
     	else {
     		
-    		totalFare = distance * 10 + time * 1;
+    		totalFare = distance * PRICE_PER_KM + time * PRICE_PER_MIN;
     	}
     	return totalFare;
 	}
 	public static void main( String[] args ) {
     	
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the Distance (Km): ");
-		System.out.println("Enter the Time (min) : ");
-		CabService cab = new CabService(2,10);
+		System.out.print("Enter the Distance (Km): ");
+		float distance = sc.nextFloat();
+		System.out.print("Enter the Time (min) : ");
+		float time = sc.nextFloat();
+		CabService cab = new CabService(distance, time);
     }
 
 	
