3371ac216db63de536ec01f4d9e663d8367cb883
vismayatk002 <vismayatk002@gmail.com>
2021-08-29 13:58:10 +0530

    Added welcome message

---
+++ src/main/java/com/cabinvoice_generator_day_30/CabService.java
@@ -0,0 +1,9 @@
+package com.cabinvoice_generator_day_30;
+
+public class CabService {
+	
+    public static void main( String[] args ) {
+    	
+        System.out.println( "Welcome to Cab Service" );
+    }
+}
